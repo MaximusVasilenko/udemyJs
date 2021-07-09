7b5dfb614f7acacabef16f7a6a833c9c366f8ad8
Maxim <maximvasil79gmail.com>
2021-07-09 16:32:00 +0300

    add if else

--- script.js
+++ script.js
@@ -44,7 +44,36 @@ personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
 
 
+if (4 == 4){
+    console.log('OK!');
+} else {
+    console.log('Error');
+}
 
 
 
+/*if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('OK');
+}*/
 
+(num === 50) ? console.log('OK') : console.log('Error');//тернарный оператор тренарный
+
+const num = '50';
+switch (num) {
+    case '49':
+        console.log('Неверно');
+        break;
+    case '100':
+        console.log('Неверно');
+        break;
+    case '51':
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
